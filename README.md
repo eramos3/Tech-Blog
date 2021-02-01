37cb9914b158bd2716e3d40d3c3e95f49c4eee7d
eramos3 <cisco@Eders-MacBook-Pro.local>
2021-01-31 17:30:54 -0800

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # Tech-Blog
+
+## Decription
+
+* GIVEN a CMS-style blog site
+WHEN I visit the site for the first time<br>
+THEN I am presented with the homepage, which includes existing blog posts if any have been posted; navigation links for the homepage and the dashboard; and the option to log in<br>
+WHEN I click on the homepage option<br>
+THEN I am taken to the homepage<br>
+WHEN I click on any other links in the navigation<br>
+THEN I am prompted to either sign up or sign in<br>
+WHEN I choose to sign up<br>
+THEN I am prompted to create a username and password<br>
+WHEN I click on the sign-up button<br>
+THEN my user credentials are saved and I am logged into the site<br>
+WHEN I revisit the site at a later time and choose to sign in<br>
+THEN I am prompted to enter my username and password<br>
+WHEN I am signed in to the site<br>
+THEN I see navigation links for the homepage, the dashboard, and the option to log out
+WHEN I click on the homepage option in the navigation<br>
+THEN I am taken to the homepage and presented with existing blog posts that include the post title and the date created<br>
+WHEN I click on an existing blog post<br>
+THEN I am presented with the post title, contents, post creator’s username, and date created for that post and have the option to leave a comment<br>
+WHEN I enter a comment and click on the submit button while signed in<br>
+THEN the comment is saved and the post is updated to display the comment, the comment creator’s username, and the date created<br>
+WHEN I click on the dashboard option in the navigation<br>
+THEN I am taken to the dashboard and presented with any blog posts I have already created and the option to add a new blog post<br>
+WHEN I click on the button to add a new blog post<br>
+THEN I am prompted to enter both a title and contents for my blog post<br>
+WHEN I click on the button to create a new blog post<br>
+THEN the title and contents of my post are saved and I am taken back to an updated dashboard with my new blog post<br>
+WHEN I click on one of my existing posts in the dashboard<br>
+THEN I am able to delete or update my post and taken back to an updated dashboard<br>
+WHEN I click on the logout option in the navigation<br>
+THEN I am signed out of the site<br>
+WHEN I am idle on the page for more than a set time<br>
+THEN I am automatically signed out of the site<br>
+## Setup
+```
+git clone git@github.com:eramos3/Tech-Blog.git
+```
+![Tech-Blog Screenshot](./public/assets/images/tech-blog-ss.png)
+
+[Tech-Blog Page](https://stormy-forest-88289.herokuapp.com/)
--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -3,103 +3,103 @@ const { Post } = require('../models');
 const postdata = [
     {
         title: 'Donec posuere metus vitae ipsum.',
-        post_desc: 'Description goes here',
+        post_desc: ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit cum, esse laboriosam quis in, est nostrum neque vitae aliquam placeat amet asperiores et commodi officiis q',
         user_id: 10
     },
     {
         title: 'Morbi non quam nec dui luctus rutrum.',
-        post_desc: 'Description goes here',
+        post_desc: ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit cum, esse laboriosam quis in, est nostrum neque vitae aliquam placeat amet asperiores et commodi officiis q',
         user_id: 8
     },
     {
         title: 'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue.',
-        post_desc: 'Description goes here',
+        post_desc: ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit cum, esse laboriosam quis in, est nostrum neque vitae aliquam placeat amet asperiores et commodi officiis q',
         user_id: 1
     },
     {
         title: 'Nunc purus.',
-        post_desc: 'Description goes here',
+        post_desc: ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit cum, esse laboriosam quis in, est nostrum neque vitae aliquam placeat amet asperiores et commodi officiis q',
         user_id: 4
     },
     {
         title: 'Pellentesque eget nunc.',
-        post_desc: 'Description goes here',
+        post_desc: ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit cum, esse laboriosam quis in, est nostrum neque vitae aliquam placeat amet asperiores et commodi officiis q',
         user_id: 7
     },
     {
         title: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
-        post_desc: 'Description goes here',
+        post_desc: ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit cum, esse laboriosam quis in, est nostrum neque vitae aliquam placeat amet asperiores et commodi officiis q',
         user_id: 4
     },
     {
         title: 'In hac habitasse platea dictumst.',
-        post_desc: 'Description goes here',
+        post_desc: ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit cum, esse laboriosam quis in, est nostrum neque vitae aliquam placeat amet asperiores et commodi officiis q',
         user_id: 1
     },
     {
         title: 'Morbi non quam nec dui luctus rutrum.',
-        post_desc: 'Description goes here',
+        post_desc: ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit cum, esse laboriosam quis in, est nostrum neque vitae aliquam placeat amet asperiores et commodi officiis q',
         user_id: 1
     },
     {
         title: 'Duis ac nibh.',
-        post_desc: 'Description goes here',
+        post_desc: ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit cum, esse laboriosam quis in, est nostrum neque vitae aliquam placeat amet asperiores et commodi officiis q',
         user_id: 9
     },
     {
         title: 'Curabitur at ipsum ac tellus semper interdum.',
-        post_desc: 'Description goes here',
+        post_desc: ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit cum, esse laboriosam quis in, est nostrum neque vitae aliquam placeat amet asperiores et commodi officiis q',
         user_id: 5
     },
     {
         title: 'In hac habitasse platea dictumst.',
-        post_desc: 'Description goes here',
+        post_desc: ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit cum, esse laboriosam quis in, est nostrum neque vitae aliquam placeat amet asperiores et commodi officiis q',
         user_id: 3
     },
     {
         title: 'Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo.',
-        post_desc: 'Description goes here',
+        post_desc: ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit cum, esse laboriosam quis in, est nostrum neque vitae aliquam placeat amet asperiores et commodi officiis q',
         user_id: 10
     },
     {
         title: 'Donec dapibus.',
-        post_desc: 'Description goes here',
+        post_desc: ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit cum, esse laboriosam quis in, est nostrum neque vitae aliquam placeat amet asperiores et commodi officiis q',
         user_id: 8
     },
     {
         title: 'Nulla tellus.',
-        post_desc: 'Description goes here',
+        post_desc: ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit cum, esse laboriosam quis in, est nostrum neque vitae aliquam placeat amet asperiores et commodi officiis q',
         user_id: 3
     },
     {
         title: 'Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo.',
-        post_desc: 'Description goes here',
+        post_desc: ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit cum, esse laboriosam quis in, est nostrum neque vitae aliquam placeat amet asperiores et commodi officiis q',
         user_id: 3
     },
     {
         title:
             'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam.',
-        post_desc: 'Description goes here',
+        post_desc: ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit cum, esse laboriosam quis in, est nostrum neque vitae aliquam placeat amet asperiores et commodi officiis q',
         user_id: 7
     },
     {
         title: 'In hac habitasse platea dictumst.',
-        post_desc: 'Description goes here',
+        post_desc: ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit cum, esse laboriosam quis in, est nostrum neque vitae aliquam placeat amet asperiores et commodi officiis q',
         user_id: 6
     },
     {
         title: 'Etiam justo.',
-        post_desc: 'Description goes here',
+        post_desc: ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit cum, esse laboriosam quis in, est nostrum neque vitae aliquam placeat amet asperiores et commodi officiis q',
         user_id: 4
     },
     {
         title: 'Nulla ut erat id mauris vulputate elementum.',
-        post_desc: 'Description goes here',
+        post_desc: ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit cum, esse laboriosam quis in, est nostrum neque vitae aliquam placeat amet asperiores et commodi officiis q',
         user_id: 6
     },
     {
         title: 'Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.',
-        post_desc: 'Description goes here',
+        post_desc: ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit cum, esse laboriosam quis in, est nostrum neque vitae aliquam placeat amet asperiores et commodi officiis q',
         user_id: 7
     }
 ];

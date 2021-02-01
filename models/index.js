77fa6b326eea8ef5f3dcbd201f50eed085e08059
eramos3 <cisco@Eders-MacBook-Pro.local>
2021-01-31 18:48:24 -0800

    fixed associations

--- models/index.js
+++ models/index.js
@@ -8,22 +8,22 @@ User.hasMany(Post, {
 });
 Post.belongsTo(User, {
     foreignKey: 'user_id',
-    onDelete: "cascade"
-});
-
-Comment.belongsTo(User, {
-    foreignKey: 'user_id'
-});
-
-Comment.belongsTo(Post, {
-    foreignKey: 'post_id'
 });
 
 User.hasMany(Comment, {
     foreignKey: 'user_id'
 });
 
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
 Post.hasMany(Comment, {
     foreignKey: 'post_id'
 });
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
 module.exports = { User, Post, Comment };
